06f9d258525945a7b45f39149903423d8a567c86
Rishabh-441 <90997234+Rishabh-441@users.noreply.github.com>
2022-01-03 00:36:21 +0530

    triangle pattern

---
+++ Recurssionques/Pattern1.java
@@ -0,0 +1,34 @@
+package RecurssionPatterns;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        triangle(4,0);
+        triangle2(4,0);
+    }
+    static void triangle2(int r, int c){
+        if (r == 0){
+            return;
+        }
+        if ( r > c){
+            triangle2(r, c+1);
+            System.out.print("*");
+        }
+        else {
+            triangle2(r-1, 0);
+            System.out.println();
+        }
+    }
+    static void triangle( int r, int c){
+        if ( r == 0){
+            return;
+        }
+        if ( r > c){
+            System.out.print("*");
+            triangle(r,c+1);
+        }
+        else{
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
+}
